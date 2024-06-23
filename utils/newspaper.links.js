a8ffe128caeb4cd2c1dd0b03506e31a6065cc149
Talut Salako <salakotalut@gmail.com>
2024-06-23 05:07:43 +0100

    add all newspaper home links

--- utils/newspaper.links.js
+++ utils/newspaper.links.js
@@ -1,4 +1,8 @@
 const newspaperData = {
-  guardian: 'https://epaper.guardian.ng'
-}
-export default newspaperData
+  guardian: "https://epaper.guardian.ng",
+  tribune: "https://tribuneonlineng.com",
+  daily_trust: "https://epaper.dailytrust.com",
+  vanguard: "https://digitalpaper.vanguardngr.com",
+  complete_sports: "https://e-paper.completesports.com/",
+};
+export default newspaperData;
