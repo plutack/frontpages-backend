ee1ce3f5f79e8f8f8c005273f7200293a5284fe3
Talut Salako <salakotalut@gmail.com>
2024-06-23 21:02:19 +0100

    abstract code and adjust daily trust paper link gen

--- utils/link.grabber.js
+++ utils/link.grabber.js
@@ -1,104 +1,117 @@
 import { load } from "cheerio";
 import puppeteer from "puppeteer-core";
 import newspaperData from "./newspaper.links.js";
+import "dotenv/config";
+
+const chromeBinaryPath = process.env.CHROME_BINARY_PATH;
 
 export const getGuardianUrl = async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_BINARY_PATH,
+    executablePath: chromeBinaryPath,
   });
-  const page = await browser.newPage();
-
-  await page.goto(newspaperData.guardian, {
-    waitUntil: "domcontentloaded",
-    timeout: 0,
-  });
-
-  const renderedHtml = await page.content();
-  const $ = load(renderedHtml);
-
-  const imgLink = $("img.pdf-thumbnail-preview").attr("src");
-  await browser.close();
-  return imgLink;
+  try {
+    const page = await browser.newPage();
+    await page.goto(newspaperData.guardian, {
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    const renderedHtml = await page.content();
+    const $ = load(renderedHtml);
+    const imgLink = $("img.pdf-thumbnail-preview").attr("src");
+    return imgLink;
+  } catch (err) {
+    console.error(`${err.name}:${err.message}`);
+  } finally {
+    await browser.close();
+  }
 };
 
 export const getTribuneUrl = async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_BINARY_PATH,
+    executablePath: chromeBinaryPath,
   });
-  const page = await browser.newPage();
-
-  await page.goto(newspaperData.tribune, {
-    waitUntil: "domcontentloaded",
-    timeout: 0,
-  });
-
-  const renderedHtml = await page.content();
-  const $ = load(renderedHtml);
-
-  const imgLink = $('img[title="Frontpage Today"]').attr("src");
-  console.log(imgLink);
-  await browser.close();
-  return imgLink;
+  try {
+    const page = await browser.newPage();
+    await page.goto(newspaperData.tribune, {
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    const renderedHtml = await page.content();
+    const $ = load(renderedHtml);
+    const imgLink = $('img[title="Frontpage Today"]').attr("src");
+    return imgLink;
+  } catch (err) {
+    console.error(`${err.name}:${err.message}`);
+  } finally {
+    await browser.close();
+  }
 };
 
 export const getDTrustUrl = async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_BINARY_PATH,
+    executablePath: chromeBinaryPath,
   });
-  const page = await browser.newPage();
-
-  await page.goto(newspaperData.daily_trust, {
-    waitUntil: "domcontentloaded",
-    timeout: 0,
-  });
-
-  const renderedHtml = await page.content();
-  const $ = load(renderedHtml);
-
-  let imgLink = $(
-    'img[fetchpriority="high"][decoding="async"][class="tp-rs-img rs-lazyload"]',
-  ).attr("src");
-  await browser.close();
-  imgLink = `https:${imgLink.replace("-scaled", "")}`;
-  return imgLink;
+  try {
+    const page = await browser.newPage();
+    await page.goto(newspaperData.daily_trust, {
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    page.setJavaScriptEnabled(false);
+    const renderedHtml = await page.content();
+    const $ = load(renderedHtml);
+    let imgLink = $(
+      'div.wp-block-themepunch-revslider img[fetchpriority="high"][decoding="async"][class="tp-rs-img rs-lazyload"]'
+    ).attr("src");
+    console.log(imgLink);
+    imgLink = `https:${imgLink.replace("-scaled", "")}`;
+    return imgLink;
+  } catch (err) {
+    console.error(`${err.name}:${err.message}`);
+  } finally {
+    await browser.close();
+  }
 };
+
 export const getVanguardUrl = async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_BINARY_PATH,
+    executablePath: chromeBinaryPath,
   });
-  const page = await browser.newPage();
-
-  await page.goto(newspaperData.vanguard, {
-    waitUntil: "domcontentloaded",
-    timeout: 0,
-  });
-
-  const renderedHtml = await page.content();
-  const $ = load(renderedHtml);
-  console.log(renderedHtml);
-
-  const imgLink = $("div.wp-block-column img").attr("src");
-  console.log(imgLink);
-  await browser.close();
-  return imgLink;
+  try {
+    const page = await browser.newPage();
+    await page.goto(newspaperData.vanguard, {
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    const renderedHtml = await page.content();
+    const $ = load(renderedHtml);
+    const imgLink = $("div.wp-block-column img").attr("src");
+    return imgLink;
+  } catch (err) {
+    console.error(`${err.name}:${err.message}`);
+  } finally {
+    await browser.close();
+  }
 };
 
 export const getSportUrl = async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_BINARY_PATH,
+    executablePath: chromeBinaryPath,
   });
-  const page = await browser.newPage();
-
-  await page.goto(newspaperData.complete_sports, {
-    waitUntil: "domcontentloaded",
-    timeout: 0,
-  });
-
-  const renderedHtml = await page.content();
-  const $ = load(renderedHtml);
-
-  let imgLink = $('head link[rel="image_src"]').attr("href");
-  await browser.close();
-  imgLink.replace(/\/\d+\/1\/\d+x\d+\/.*\.jpg$/, "/1135x1600/");
-  return imgLink;
+  try {
+    const page = await browser.newPage();
+    await page.goto(newspaperData.complete_sports, {
+      waitUntil: "domcontentloaded",
+      timeout: 0,
+    });
+    const renderedHtml = await page.content();
+    const $ = load(renderedHtml);
+    let imgLink = $('head link[rel="image_src"]').attr("href");
+    imgLink.replace(/\/\d+\/1\/\d+x\d+\/.*\.jpg$/, "/1135x1600/");
+    return imgLink;
+  } catch (err) {
+    console.error(`${err.name}:${err.message}`);
+  } finally {
+    await browser.close();
+  }
 };
