175a8402e41c7a567244ae90753dc427a2c631e6
Talut Salako <salakotalut@gmail.com>
2024-06-23 05:26:07 +0100

    fix: lint code

--- utils/link.grabber.js
+++ utils/link.grabber.js
@@ -1,23 +1,23 @@
-import { load } from 'cheerio'
-import puppeteer from 'puppeteer-core'
-import newspaperData from './newspaper.links.js'
+import { load } from "cheerio";
+import puppeteer from "puppeteer-core";
+import newspaperData from "./newspaper.links.js";
 
 export const getGuardianUrl = async () => {
   const browser = await puppeteer.launch({
-    executablePath: process.env.CHROME_BINARY_PATH
-  })
-  const page = await browser.newPage()
+    executablePath: process.env.CHROME_BINARY_PATH,
+  });
+  const page = await browser.newPage();
 
   // Navigate the page to a URL.
   await page.goto(newspaperData.guardian, {
-    waitUntil: 'load',
-    timeout: 0
-  })
+    waitUntil: "load",
+    timeout: 0,
+  });
 
-  const renderedHtml = await page.content()
-  const $ = load(renderedHtml)
+  const renderedHtml = await page.content();
+  const $ = load(renderedHtml);
 
-  const imgLink = $('img.pdf-thumbnail-preview').attr('src')
-  await browser.close()
-  return imgLink
-}
+  const imgLink = $("img.pdf-thumbnail-preview").attr("src");
+  await browser.close();
+  return imgLink;
+};
