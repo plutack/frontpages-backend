9adeea4884f3188f02b21e7148d8a2478efe3692
Talut Salako <salakotalut@gmail.com>
2024-06-23 05:07:43 +0100

    add all newspaper home links

--- utils/cloudinary.operation.js
+++ utils/cloudinary.operation.js
@@ -1,23 +1,23 @@
-import { v2 as cloudinary } from 'cloudinary'
-import moment from 'moment'
-import 'dotenv/config'
+import { v2 as cloudinary } from "cloudinary";
+import moment from "moment";
+import "dotenv/config";
 
 // Configuration
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET
-})
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 // Upload an image
 export const uploadImg = async (newspaperName, imgLink) => {
   try {
     const { secure_url } = await cloudinary.uploader.upload(imgLink, {
-      public_id: `${newspaperName}-${moment().format('DD-MM-YYYY')}`
-    })
-    return secure_url
+      public_id: `${newspaperName}-${moment().format("DD-MM-YYYY")}`,
+    });
+    return secure_url;
   } catch (error) {
-    console.error(error)
-    return ''
+    console.error(error);
+    return "";
   }
-}
+};
