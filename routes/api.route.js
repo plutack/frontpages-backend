f99bf8120f9e94af34d5cddef1d153428111dd6d
Talut Salako <salakotalut@gmail.com>
2024-06-25 12:28:00 +0100

    fix: typo error

--- routes/api.route.js
+++ routes/api.route.js
@@ -24,7 +24,7 @@ apiRoute.get("/api/today", async (req, res) => {
     if (err.message === "no data available") {
       return res.json({ success: false, message: "updates coming soon" });
     }
-    return res.staus(400).json({ success: false, message: err.message });
+    return res.status(400).json({ success: false, message: err.message });
   }
 });
 
