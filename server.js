9e3c2104e4a03a4d1875193f5ac9159b70bd3345
Talut Salako <salakotalut@gmail.com>
2024-06-28 21:13:15 +0100

    separate cron job and express server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 import app from "./index.js";
-import job from "./cron/job.js";
 import "dotenv/config";
 
 const uri = process.env.MONGODB_URL;
@@ -10,7 +9,6 @@ try {
   await mongoose.connect(uri, {
     serverApi: { version: "1", strict: true, deprecationErrors: true },
   });
-  job.start();
   console.log("cron job started");
   app.listen(port, () => {
     console.log(`App started on port: ${port}`);
