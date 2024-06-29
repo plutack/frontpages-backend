0d45498c13bf6f636a498f650f3b837a0a0eb41b
Talut Salako <salakotalut@gmail.com>
2024-06-29 12:05:13 +0100

    updated database save function

--- cron/job.js
+++ cron/job.js
@@ -1,5 +1,7 @@
 import { CronJob } from "cron";
+import mongoose from "mongoose";
 import newspaperData from "../utils/newspaper.links.js";
+import moment from "moment";
 import {
   getGuardianUrl,
   getTribuneUrl,
@@ -13,28 +15,54 @@ import Entry from "../model/entry.js";
 import "dotenv/config";
 
 const time = process.env.CRON_TIME;
+const uri = process.env.MONGODB_URL;
 
+const date = moment().format("YYYY-MM-DD");
 const newspaperNames = Object.keys(newspaperData);
 let newspapers = [];
 
 const saveToArray = async (newspaperName, urlGrabberFunction) => {
   const imgUrl = await urlGrabberFunction();
   if (!imgUrl) {
+    console.log(`No link for ${newspaperName}`);
     return;
   }
   const uploadedLink = await uploadImg(newspaperName, imgUrl);
   const newspaperInfo = { name: newspaperName, link: uploadedLink };
   newspapers.push(newspaperInfo);
+  console.log("newspaper saved", newspaperInfo);
 };
 
-const job = new CronJob(time, async () => {
+async function saveOrUpdateEntry() {
+  const dbConnection = await mongoose.connect(uri, {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
+  });
   try {
-    console.log(newspapers);
+    const existingEntry = await Entry.findOne({ date });
+
+    if (!existingEntry) {
+      const entry = new Entry({ date, newspapers });
+      await entry.save();
+      console.log(`New entry for date: ${date} saved to database`);
+    } else {
+      await Entry.findOneAndUpdate({ date }, { newspapers });
+      console.log(`Existing entry for date: ${date}  updated in database`);
+    }
+  } catch (error) {
+    console.error("Error connecting to database or saving entry:", error);
+  } finally {
+    await dbConnection.disconnect();
+  }
+}
+
+const job = new CronJob(time, async () => {
+  console.log("cron job started");
+  try {
     console.log("started fetching data");
     for (const newspaperName of newspaperNames) {
       switch (newspaperName) {
         case "guardian":
-          // await saveToArray(newspaperName, getGuardianUrl);
+          await saveToArray(newspaperName, getGuardianUrl);
           break;
         case "tribune":
           // await saveToArray(newspaperName, getTribuneUrl);
@@ -43,7 +71,7 @@ const job = new CronJob(time, async () => {
           // await saveToArray(newspaperName, getDTrustUrl);
           break;
         case "vanguard":
-          await saveToArray(newspaperName, getVanguardUrl);
+          // await saveToArray(newspaperName, getVanguardUrl);
           break;
         case "complete_sports":
           // await saveToArray(newspaperName, getSportUrl);
@@ -52,11 +80,7 @@ const job = new CronJob(time, async () => {
           break;
       }
     }
-    const entry = new Entry();
-    entry.newspapers = newspapers;
-    console.log(entry);
-    // const response = await entry.save();
-    console.log(response);
+    saveOrUpdateEntry();
   } catch (err) {
     console.error(`${err.name}:${err.message}`);
   } finally {
