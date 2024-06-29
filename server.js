5754fc895d5210723a41d9baa17aa80789768e71
Talut Salako <salakotalut@gmail.com>
2024-06-29 12:05:13 +0100

    updated database save function

--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-import mongoose from "mongoose";
 import app from "./index.js";
 import "dotenv/config";
 
-const uri = process.env.MONGODB_URL;
+
 const port = process.env.PORT;
 
 try {
-  await mongoose.connect(uri, {
-    serverApi: { version: "1", strict: true, deprecationErrors: true },
-  });
-  console.log("cron job started");
   app.listen(port, () => {
     console.log(`App started on port: ${port}`);
   });
