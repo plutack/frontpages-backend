64b603667f8046879a3ceb75f5588a3654854e76
Talut Salako <salakotalut@gmail.com>
2024-06-30 22:53:15 +0100

    add frontend files

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import apiRoute from "./routes/api.route.js";
 
 const app = express();
 
+app.use(express.static("public"));
+
 app.use("/", apiRoute);
 
 export default app;
--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import app from "./index.js";
 import "dotenv/config";
-
+import mongoose from "mongoose";
 
 const port = process.env.PORT;
+const uri = process.env.MONGODB_URL;
 
-try {
-  app.listen(port, () => {
-    console.log(`App started on port: ${port}`);
+const startServer = async () => {
+  await mongoose.connect(uri, {
+    serverApi: { version: "1", strict: true, deprecationErrors: true },
   });
-} catch (err) {
-  console.log("Error", err);
-}
+  try {
+    app.listen(port, () => {
+      console.log(`App started on port: ${port}`);
+    });
+  } catch (err) {
+    console.log("Error", err);
+  }
+};
+
+await startServer();
