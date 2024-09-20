9b81d69b1769c3243c826c70fa5097bc765fbe60
Talut Salako <salakotalut@gmail.com>
2024-09-20 20:20:27 +0100

    feat: add hash file checks to prevent duplicate api calls

---
+++ utils/hash.functions.js
@@ -0,0 +1,27 @@
+import {createHash} from "crypto";
+
+
+// convert image to data url string via link to image
+export async function urlToBase64(url) { 
+    const response = await fetch(url)
+    if (!response.ok) throw new Error("failed to get image")
+    const arrayBuffer = await response.arrayBuffer()
+    const buffer = Buffer.from(arrayBuffer)
+    return buffer.toString("base64")
+}
+
+// get hash from data url string
+export async function  calculateHash(dataUrlString){
+    const hash = createHash('SHA-256'); 
+    hash.update(dataUrlString)
+    return hash.digest("hex")
+}
+
+export function compareHash(firstHash, secondHash){
+  if (firstHash === secondHash){
+    return true
+  }
+  return false
+}
+
+
