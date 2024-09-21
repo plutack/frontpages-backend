d04634b65d7eaffda3841092cf390cd540979cd3
Talut Salako <salakotalut@gmail.com>
2024-09-21 08:21:34 +0100

    refactor: remove redundant code

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
 import app from "./index.js";
-import "dotenv/config";
-import mongoose from "mongoose";
-import job from "./cron/job.js";
 
 const port = process.env.PORT;
-const uri = process.env.MONGODB_URL;
 
-const startServer = async () => {
-  console.log("Starting job");
-  job.start();
-  await mongoose.connect(uri, {
-    serverApi: { version: "1", strict: true, deprecationErrors: true },
-  });
+const startServer =  () => {
+  // console.log("Starting job");
+  // job.start();
   try {
     app.listen(port, () => {
       console.log(`App started on port: ${port}`);
     });
   } catch (err) {
-    console.log("Error", err);
+    console.log("Error", err.message);
   }
 };
 
-await startServer();
+startServer();
