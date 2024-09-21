d651968fa13a7e77d6dfc924239ebb5b09fafca0
Talut Salako <salakotalut@gmail.com>
2024-09-21 15:02:23 +0100

    refactor: make index.js entry file

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 import newspaper from "./routes/newspaper.js";
 import {Hono} from "hono"
+import {serve} from "@hono/node-server"
+import mongoose from "mongoose"
+
 const app = new Hono();
 
+app.route("/newspapers", newspaper);
 
-app.use("/newspapers", newspaper);
 
-export default app;
+const port = process.env.PORT;
+const uri = process.env.MONGODB_URL;
+
+const connectToDatabase =  async () => {
+    
+    try {
+        await mongoose.connect(uri, {
+            serverApi: { version: "1", strict: true, deprecationErrors: true },
+          });
+    } catch (err) {
+      console.log("Error", err);
+    }
+  };
+
+  
+await connectToDatabase()
+console.log(`server started at ${port}`)
+serve({
+  fetch: app.fetch,
+  port
+})
